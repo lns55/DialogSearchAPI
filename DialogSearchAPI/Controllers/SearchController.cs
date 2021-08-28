de672d9b5b6786a22b16aa6b166cf96932cbecf9
lzn747 <lzn747@gmail.com>
2021-08-28 10:38:54 +0300

    Search controller created.

--- DialogSearchAPI/Controllers/SearchController.cs
+++ DialogSearchAPI/Controllers/SearchController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using iMessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DialogSearchAPI.Controllers
 {
-   
-    
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly IEnumerable<RGDialogsClients> _data;
+
+        public SearchController(IEnumerable<RGDialogsClients> data)
+        {
+            _data = data;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        public ActionResult<Guid> Post(ICollection<Guid> clientId)
+        {
+            var dialogSearch = _data
+               .GroupBy(x => x.IDRGDialog)
+               .Where(x => x.All(x => clientId.Contains(x.IDClient))
+                           && x.Count() == clientId.Count)
+               .Select(x => x.Key)
+               .SingleOrDefault();
+
+            return dialogSearch != default
+                ? Ok(dialogSearch)
+                : NotFound();
+        }
+    }
 }
+
