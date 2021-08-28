e466bc208c1d575fe9d29c73b26d8528ebadd3d1
lzn747 <lzn747@gmail.com>
2021-08-28 10:13:11 +0300

    Добавьте файлы проекта.

---
+++ DialogSearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DialogSearchAPI.Controllers
+{
+   
+    
+}
